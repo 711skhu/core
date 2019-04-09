e908921cd100e2f0eac696648fae2554f51eb2fc
HYE_JI <lwiiiiii@naver.com>
2019-04-09 18:58:14 +0900

    service class 작성

---
+++ src/main/java/com/shouwn/oj/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.shouwn.oj.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public abstract class MemberService implements UserDetailsService {
+}
