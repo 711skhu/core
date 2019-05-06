409e04ca44abd7ff376fbdf25cc1feb37b365461
HYE_JI <lwiiiiii@naver.com>
2019-05-06 17:27:50 +0900

    생성자 추가

--- src/main/java/com/shouwn/oj/model/request/process/ProcessRequest.java
+++ src/main/java/com/shouwn/oj/model/request/process/ProcessRequest.java
@@ -3,14 +3,24 @@ package com.shouwn.oj.model.request.process;
 import java.util.List;
 
 import com.shouwn.oj.model.entity.problem.TestCase;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProcessRequest {
+
 	private String sourceCode;
 	private String language;
 	private List<TestCase> testCases;
 	private long pk;
+
+	@Builder
+	public ProcessRequest(String sourceCode, String language, List<TestCase> testCases, long pk) {
+		this.sourceCode = sourceCode;
+		this.language = language;
+		this.testCases = testCases;
+		this.pk = pk;
+	}
 }
