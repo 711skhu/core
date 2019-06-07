3bc66526d5c4232e846ac95a6f177c8626e745fd
yejilee <yjll1019@naver.com>
2019-06-07 17:49:12 +0900

    findByProblemDetailIdOrderById 메소드 생성

--- src/main/java/com/shouwn/oj/repository/problem/TestCaseRepository.java
+++ src/main/java/com/shouwn/oj/repository/problem/TestCaseRepository.java
@@ -1,8 +1,12 @@
 package com.shouwn.oj.repository.problem;
 
+import java.util.List;
+
 import com.shouwn.oj.model.entity.problem.TestCase;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TestCaseRepository extends JpaRepository<TestCase, Long>, TestCaseRepositoryCustom {
+	List<TestCase> findByProblemDetailIdOrderById(long problemDetailId);
 }
+
--- src/main/java/com/shouwn/oj/service/problem/TestCaseService.java
+++ src/main/java/com/shouwn/oj/service/problem/TestCaseService.java
@@ -1,7 +1,21 @@
 package com.shouwn.oj.service.problem;
 
+import java.util.List;
+
+import com.shouwn.oj.model.entity.problem.TestCase;
+import com.shouwn.oj.repository.problem.TestCaseRepository;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestCaseService {
+	private final TestCaseRepository testCaseRepository;
+
+	public TestCaseService(TestCaseRepository testCaseRepository) {
+		this.testCaseRepository = testCaseRepository;
+	}
+
+	public List<TestCase> findByProblemDetailIdOrderById(long problemDetailId) {
+		return testCaseRepository.findByProblemDetailIdOrderById(problemDetailId);
+	}
 }
