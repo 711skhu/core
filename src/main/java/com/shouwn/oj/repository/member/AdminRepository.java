91d21d353a78911410d34f01eca88501f6ac4717
shouwn <sjonghyon@gmail.com>
2019-06-09 19:11:24 +0900

    PR리뷰 - 레퍼지토리 상속 수정

--- src/main/java/com/shouwn/oj/repository/member/AdminRepository.java
+++ src/main/java/com/shouwn/oj/repository/member/AdminRepository.java
@@ -2,5 +2,5 @@ package com.shouwn.oj.repository.member;
 
 import com.shouwn.oj.model.entity.member.Admin;
 
-public interface AdminRepository extends StudentRepositoryCustom, MemberRepository<Admin, Long> {
+public interface AdminRepository extends AdminRepositoryCustom, MemberRepository<Admin, Long> {
 }
