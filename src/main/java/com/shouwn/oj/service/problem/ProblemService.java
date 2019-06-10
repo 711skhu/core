c2d3d6ff44ca611060bfecad4742f6826da6a3bc
HYE_JI <lwiiiiii@naver.com>
2019-06-10 18:31:14 +0900

    사용하지 않는 코드 삭제

--- src/main/java/com/shouwn/oj/service/problem/ProblemService.java
+++ src/main/java/com/shouwn/oj/service/problem/ProblemService.java
@@ -1,22 +1,7 @@
 package com.shouwn.oj.service.problem;
 
-import java.util.Optional;
-
-import com.shouwn.oj.model.entity.problem.Problem;
-import com.shouwn.oj.repository.problem.ProblemRepository;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProblemService {
-
-	private ProblemRepository problemRepository;
-
-	public ProblemService(ProblemRepository problemRepository) {
-		this.problemRepository = problemRepository;
-	}
-
-	public Optional<Problem> findById(Long problemId) {
-		return problemRepository.findById(problemId);
-	}
 }
